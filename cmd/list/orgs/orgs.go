d08c313affce669e2e9b18c7b1d0ac0d4c3b8e7d
Ben Zarboni <99673202+bzarboni1@users.noreply.github.com>
2024-05-27 09:14:45 -0400

    feature: Add status checks to the cli (#4)
    * feature: Add status checks to the cli

---
+++ cmd/list/orgs/orgs.go
@@ -0,0 +1,57 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package list
+
+import (
+	"errors"
+	"fmt"
+	"gh_foundations/internal/pkg/functions"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var OrgsCmd = &cobra.Command{
+	Use:   "orgs",
+	Short: "List managed organizations.",
+	Long: `List managed organizations. This command will list all organizations.\n
+	found in the state file.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("requires the path of the \"organizations\" directory")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		
+		orgsDir := args[0]
+
+		orgs, err := functions.FindManagedOrgs(orgsDir)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		var orgOut string
+		orgOut = ""
+		for _, org := range orgs {
+			if orgOut != "" {
+				orgOut = fmt.Sprintf("%s, '%s'", orgOut, org)
+			} else {
+				orgOut = fmt.Sprintf("['%s'", org)
+			}
+		}
+		orgOut = fmt.Sprintf("%s]", orgOut)
+
+		fmt.Println(orgOut)
+
+	},
+}
+
+func init() {
+	os.Setenv("GCP_SECRET_MANAGER_PROJECT", "blahblah")
+	os.Setenv("GCP_TF_STATE_BUCKET_PROJECT", "blahblahblah")
+	os.Setenv("GCP_TF_STATE_BUCKET_NAME", "blahblahblahblah")
+	os.Setenv("GCP_TF_STATE_BUCKET_LOCATION", "blahblahblahblahblah")
+}
